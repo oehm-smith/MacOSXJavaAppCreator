df4da15da3f3f782ae7e9ee7930ee055e4da467b
Brooke Smith <brooke@tintuna.com>
2013-08-13 22:07:52 +1000

    Yet another github project underway

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ MacOSXJavaAppCreator
 ====================
 
 Nothing too amazing here.  Just a script or two and some instructions on creating Java Apps for Macs using Maven.  
+
+First is the script buildIcns.sh that uses 'sips' to create icon sized files (all Mac systems have this installed on it).  
+
+Next is the advice to use Maven and add the [http://mojo.codehaus.org/osxappbundle-maven-plugin](http://mojo.codehaus.org/osxappbundle-maven-plugin) to create the .dmg file.
+
+I will modify the script to use Maven properties for the location of the graphic etc...
+
---
+++ buildIcns.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Create the JStock.icns resource as used by MacOSX
+GRAPHIC=../../target/classes/images/splash/jstock2-wording.png
+ICONS_DIR=jstock.icons
+ICNS=./jstock.icns
+LOCALFILE=./input_image
+
+# Create the graphics
+if [ ! -d $ICONS_DIR ]; then
+	mkdir $ICONS_DIR
+else 
+	rm -r $ICONS_DIR/*
+fi
+if [ ! -d $LOCALFILE ]; then
+	mkdir $LOCALFILE
+else 
+	rm -r $LOCALFILE/*
+fi
+LOCALFILE=$LOCALFILE/`basename $GRAPHIC`
+cp $GRAPHIC $LOCALFILE
+for i in 16 32 128 256 512
+do
+	CMD="sips -z $i $i $LOCALFILE --out $ICONS_DIR/icon_${i}x${i}.png"
+	echo CMD: $CMD
+	`$CMD`
+done
+
+# Create the icns resource
+#iconutil -c icns $ICONS_DIR
+
+
